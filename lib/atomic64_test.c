554e4e01ccaa00a39bb455e7306dbafe96e837db
Borislav Petkov (AMD) <bp@alien8.de>
2022-12-20 11:38:08 +0100

    Convert X86_FEATURE_CX8
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>

--- lib/atomic64_test.c
+++ lib/atomic64_test.c
@@ -259,7 +259,7 @@ static __init int test_atomics_init(void)
 #else
 		"i386+",
 #endif
-	       boot_cpu_has(X86_FEATURE_CX8) ? "with" : "without",
+	       cpuid_info.f1.cx8 ? "with" : "without",
 	       boot_cpu_has(X86_FEATURE_XMM) ? "with" : "without");
 #else
 	pr_info("passed\n");
